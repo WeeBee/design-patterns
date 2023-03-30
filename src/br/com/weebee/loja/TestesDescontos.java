da72196b65d035ae4757e81d3b46138449d0bd5a
Wilson <wilsjunior@f1rst.com.br>
2023-03-30 16:51:23 -0300

    add .gitignore for office environment changes

--- src/br/com/weebee/loja/TestesDescontos.java
+++ src/br/com/weebee/loja/TestesDescontos.java
@@ -6,15 +6,17 @@ import br.com.weebee.loja.desconto.CalculadoraDeDescontos;
 import br.com.weebee.loja.orcamento.Orcamento;
 
 public class TestesDescontos {
+	final static BigDecimal valor1 = new BigDecimal("200");
+	final static BigDecimal valor2 = new BigDecimal("1000");
 
 	//Chain of Responsibility
 	public static void main(String[] args) {
-		Orcamento primeiro = new Orcamento(new BigDecimal("200"), 6);
-		Orcamento segundo = new Orcamento(new BigDecimal("1000"), 1);
+		Orcamento primeiro = new Orcamento(valor1, 6);
+		Orcamento segundo = new Orcamento(valor2, 1);
 
 		CalculadoraDeDescontos calculadora = new CalculadoraDeDescontos();
-		System.out.println("desconto com 6 itens de 200 = " + calculadora.calcular(primeiro));
-		System.out.println("desconto com 1 item de 1000 = " + calculadora.calcular(segundo));
+		System.out.println("desconto em 6 itens de R$ " + valor1 + " = " + calculadora.calcular(primeiro));
+		System.out.println("desconto em 1 item de R$ " + valor2 + " = " + calculadora.calcular(segundo));
 	}
 
 }
